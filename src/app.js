748c0c5a3b84dd8d7de6a130a4860e8a0ab290ba
ChelseaVK <cvanklaveren5@gmail.com>
2025-06-10 12:28:32 -0500

    Added the forecast HTML from Javascript.

--- src/app.js
+++ src/app.js
@@ -54,3 +54,28 @@ let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Fort Worth");
+
+function displayForecast() {
+  let forecast = document.querySelector("#forecast");
+
+  let days = ["Tue", "Wed", "Thurs", "Fri", "Sat"];
+  let forecastHtml = "";
+
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      `<div class="weather-forecast-day">
+    <div class="weather-forecast-date">${day}</div>
+    <div class="weather-forecast-icon">🌤️</div>
+    <div class="weather-forecast-temperatures">
+      <div class="weather-forecast-temperature">
+        <strong>15º</strong>
+      </div>
+      <div class="weather=forecast-temperature">9°</div>
+      </div>`;
+  });
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHtml;
+}
+
+displayForecast();
